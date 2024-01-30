2f0ec07412fc82a3b6dd18d80ca12d7b87d59d48
JavierYami <javieryami1@outlook.com>
2024-01-30 15:16:34 -0600

    prototipo info no responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from './components/Header/Header'
+import Info from './components/Info/Info'
 
 function App() {
 
   return (
     <>
       <Header></Header>
+      <Info></Info>
     </>
   )
 }
---
+++ src/components/Info/Info.jsx
@@ -0,0 +1,56 @@
+import './info.css'
+
+const Info = () => {
+    
+    return (
+        <div className='mainContainer'>
+            <div className='titleContainer'>
+                <h1 className='title'>Brasileiro: Paga mais de R$400? Agora pode receber 10 VEZES MAIS com este bônus de energias limpas!</h1>
+            </div>
+            <div className='infoContainer'>
+                <div className='info'>
+                    <h3 className='infoTitle'>A porta para economia está na sua casa</h3>
+                    <br />
+                    <p className='infoText'>Já pensou na possibilidade de instalar painéis solares na sua casa? Se já o fez, isso vai convencê-lo! Pode gerar até 2.000 kWh e, com isso, tornar-se mais próspero.
+                    <br />
+                    Foi anunciado no Brasil um benefício acessível para quase toda a população economicamente ativa.
+                    <br />
+                    Se você nasceu entre 1941 e 1991, pode começar a gerar ganhos graças ao aumento das energias limpas e ao interesse dos líderes em criar um ambiente mais sustentável.
+                    <br/>
+                    A maioria dos proprietários ainda acredita que um sistema de energia solar está fora do seu alcance... mas, e se lhe dissermos que não é? Na realidade, somando os incentivos e economias na conta de eletricidade, o maior beneficiário é o brasileiro comum. Muitos já estão ganhando R$4000, e hoje pode ser o próximo.</p>
+                </div>
+                <img className='imgSavings' src="https://cmps-blanco.s3.amazonaws.com/pt/panels/solLivre_brasil/assets/pig-f700c9b5.webp" alt="cochinito"/>
+            </div>
+            <div className='beneficiosContainer'>
+                <div className='beneficiosImg'>
+
+                </div>
+                <div className='beneficiosInfoContainer'>
+                    <div className='info'>
+                        <h3>Conheça os incríveis benefícios para os brasileiros entre 32 e 82 anos</h3>
+                        <br />
+                        <ul className='lista'>
+                            <li>Os painéis solares podem aumentar o valor da sua casa</li>
+                            <br />
+                            <li>Pode ganhar R$4000 em pouco tempo e sem sair de casa!</li>
+                            <br />
+                            <li>Pode congelar o preço da sua conta de eletricidade e poupar ainda mais!</li>
+                            <br />
+                            <li>Demonstra o seu compromisso e interesse em melhorar as condições ambientais</li>
+                            <br />
+                            <li>Ao tornar-se autossuficiente em energia, terá a tranquilidade de não depender de ninguém</li>
+                            <br />
+                        </ul>
+                        <br />
+                        <h3>Quer saber como a instalação de energia solar pode render até R$4000?</h3>
+                        <br />
+                        <button>Clique Aqui</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+
+}
+
+export default Info;
